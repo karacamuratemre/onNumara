051510d54e2580469c6e637151b5c65218e9411d
karacamuratemre <karacamuratemre@gmail.com>
2024-08-27 01:39:17 +0300

    first

---
+++ src/main/java/com/example/onnumara/OnNumaraApplication.java
@@ -0,0 +1,13 @@
+package com.example.onnumara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnNumaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnNumaraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onnumara/OnNumaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onnumara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnNumaraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
